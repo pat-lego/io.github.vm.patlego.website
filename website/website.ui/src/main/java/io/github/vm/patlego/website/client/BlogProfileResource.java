8698d3cdd224d4de3bf634fa67363d3db1cf878c
Patrique Legault <patrique.legault@perficient.com>
2020-12-01 21:24:07 -0500

    Adding blog profile resource

---
+++ website/website.ui/src/main/java/io/github/vm/patlego/website/client/BlogProfileResource.java
@@ -0,0 +1,17 @@
+package io.github.vm.patlego.website.client;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * This resource will allow URL's to navigate straight to the profile/contact page and not have to click on 
+ * the Contact button to be routed to the Contact page
+ */
+@Component(immediate = true, 
+    service = BlogProfileResource.class, 
+    property = { 
+        "osgi.http.whiteboard.resource.pattern=/profile/blog/*", 
+        "osgi.http.whiteboard.resource.prefix=/profile"
+    })
+public class BlogProfileResource {
+
+}
