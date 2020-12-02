a52cf838ae5b9424880e506fff00e3f574ba6c17
Patrique Legault <patrique.legault@perficient.com>
2020-12-02 13:37:19 -0500

    Adding ability to get blog

--- website/website.ui/src/main/resources/website.ui/src/views/Blog.vue
+++ website/website.ui/src/main/resources/website.ui/src/views/Blog.vue
@@ -1,26 +1,52 @@
 <template>
-    <div class="flex flex-col">
+    <div v-if="data" class="flex flex-col">
         <div class="w-1/4">
             <div class="flex content-center pl-10 pb-5 text-xl">
-                {{blog.blogTitle}}
+                {{data.blogTitle}}
             </div>
         </div>
         <div class="flex w-full justify-center content-center">
             <div class="w-3/4 p-20 bg-gray-100">
-                <span v-html="blog.blog"></span>
+                <span v-html="data.blog"></span>
             </div>
         </div>
          <div class="w-1/4">
             <div class="flex content-center pl-10 pt-5 italic">
-                By: {{blog.blogAuthor}}
+                By: {{data.blogAuthor}}
             </div>
          </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   props: {
     blog: Object
+  },
+  data: () => {
+    return {
+      data: undefined
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      if (this.blog) {
+        this.data = this.blog
+      } else {
+        if (process.env.NODE_ENV === 'development') {
+          axios
+            .get(`http://localhost:8181/cxf/patlegovm/1.0/site/blogs/${this.$route.params.id}`)
+            .then(response => { this.data = response.data })
+        } else {
+          axios
+            .get(`/cxf/patlegovm/1.0/site/blogs/${this.$route.params.id}`)
+            .then(response => { this.data = response.data })
+        }
+      }
+    }
   }
 }
 </script>
