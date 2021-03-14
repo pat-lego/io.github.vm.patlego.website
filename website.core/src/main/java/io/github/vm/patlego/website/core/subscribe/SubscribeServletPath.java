48c64225651e4618618fb7f92296c7b5b852809d
pat-lego <patrique.legault@gmail.com>
2021-03-13 21:30:21 -0500

    Adding fixes to the refacroting

--- website.core/src/main/java/io/github/vm/patlego/website/core/subscribe/SubscribeServletPath.java
+++ website.core/src/main/java/io/github/vm/patlego/website/core/subscribe/SubscribeServletPath.java
@@ -3,5 +3,5 @@ package io.github.vm.patlego.website.core.subscribe;
 import io.github.vm.patlego.website.core.SiteServletPath;
 
 public class SubscribeServletPath extends SiteServletPath {
-    public final static String SUBSCRIBE_METHOD_PATH = "/subscribe";
+    public static final String SUBSCRIBE_METHOD_PATH = "/subscribe";
 }
